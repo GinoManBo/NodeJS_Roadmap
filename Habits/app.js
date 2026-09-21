4399adc48414a3888c9d95f9c4e8db97c2cf6f36
GinoManBo <monster.alfreval.yoshy@gmail.com>
2026-09-21 19:15:16 -0300

    Avance en habits funcion comparison

--- Habits/app.js
+++ Habits/app.js
@@ -6,6 +6,23 @@ app.use(express.json());
 let habits = [];
 let nextId = 1;
 
+function comparison(iso, habit) { // iso viene como new Date();
+
+  const iso = iso.toISOString().substring(0, 10); // YYYY-MM-DD
+  const today = new Date().toISOString().substring(0, 10); // YYYY-MM-DD
+  if (iso.substring(0,3) > today.substring(0,3)) { // verificacion año
+    if (Number(iso.substring(5,7)) + 12 == 13 && Number(today.substring(5,7)) == 1) { // verificacion mes por cambio de año
+      habit.lastCheckIn = (today);
+      habit.checkins.push(today);
+      habit.streak = habit.streak++;
+
+      return res.json(habit);
+    } else if (Number(today.)) {
+      
+    }
+  }
+}
+
 /*
 {
   id: number,
@@ -52,9 +69,12 @@ app.post("/api/habits", (req, res) => {
   
 });
 
+
 app.post("/api/habits/:id/checkin", (req, res) => {
   const id = Number(req.params.id);
   const habit = habits.find(habit => habit.id === id);
+
+  
 
   if (!habit) {
     return res.status(404).json([
@@ -65,13 +85,13 @@ app.post("/api/habits/:id/checkin", (req, res) => {
   }
   
   if (habit.lastCheckIn === null) {
-    habit.lastCheckIn = new Date.now();
+    habit.lastCheckIn.push(new Date().toISOString().substring(0, 10));
     habit.streak = habit.streak++;
   } else if (new Date().toISOString().substring(0, 10) - habit.lastCheckIn.toISOString().substring(0, 10) === 1) {
     habit.streak = habit.streak++;
     checkins.push(new Date().toISOString().substring(0, 10));
   } else {
-    
+    habit.streak = 0;
   }
   
 });
